4ef3a218e9ef8cfcf451dca842d5827b3399fc66
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-10-05 00:38:12 +0530

    lesson 2 exercises

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,15 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get(() => {})
+  .post(() => {})
+
+router
+  .route('/:id')
+  .get(() => {})
+  .put(() => {})
+  .delete(() => {})
+
 export default router
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
-
+import ItemRouter from './resources/item/item.router'
 export const app = express()
 
 app.disable('x-powered-by')
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/api/item', ItemRouter)
 
 export const start = async () => {
   try {
